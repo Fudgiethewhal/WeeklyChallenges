5f87a00986977c18f8cdd9b8e70041c22d33d93c
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-01-16 18:17:41 -0800

    completed sets

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -16,20 +16,32 @@ namespace ChallengesWithTestsMark8
                 }
                 else
                 {
-                        return true;
+                    return true;
                 }
             }
+
             return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
             if (numbers == null)
-            { return false; }
+            {
+                return false;
+            }
 
             int numbersSum = numbers.Sum();
-            if
+            if (numbersSum % 2 != 0)
+            {
+                return true;
+            }
+
+            {
+                return false;
+            }
         }
+    
+
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
@@ -53,18 +65,32 @@ namespace ChallengesWithTestsMark8
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    list.Add(i);
+                }
+            }
+        return list.ToArray();
         }
+        
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
--- ChallengesWithTestsMark8/obj/Debug/net8.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net8.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be76c41fbb8f28ddb0d0fa04db06585d643b538a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd67f6a0f0b46689ec0dbc778d78477910d7ce5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
