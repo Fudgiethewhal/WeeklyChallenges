d0cef34918ecff3bdcbab0610e6f20cf6244d13c
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-02-06 20:28:14 -0800

    finished 3 more

--- ChallengesWithTestsMark8/obj/Debug/net8.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net8.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b772a504867cd1a83db615a16a6803692016088a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abfef28f534ebd0a126217d8feb8cb545fd79ebb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
